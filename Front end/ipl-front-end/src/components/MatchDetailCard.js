9b2eb21461f61d4bdee8d76bb8fd798a047df8af
vishal <vishalambekar459@gmail.com>
2023-09-29 12:52:08 +0530

    front end connection done to backend

--- Front end/ipl-front-end/src/components/MatchDetailCard.js
+++ Front end/ipl-front-end/src/components/MatchDetailCard.js
@@ -1,10 +1,13 @@
 import {React} from 'react'
 
-export const MatchDetailCard = () => {
+export const MatchDetailCard = ({match}) => {
+  if(!match) return null;  //If this is removed will get error
+
   return (
     <div className="MatchDetailCard">
       <h3>Latest matches</h3>
       <h4>Match Details</h4>
+      <h4>{match.team1} vs {match.team2}</h4>
       
     </div>
   );
--- Front end/ipl-front-end/src/pages/TeamPage.js
+++ Front end/ipl-front-end/src/pages/TeamPage.js
@@ -3,28 +3,31 @@ import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
-  const [Team, setTeam] = useState();
+  const [team, setTeam] = useState({matches : []}); //{} is to initialize team with empty ..matches also
+  const reptiles = ["alligator", "snake", "lizard"];
 
   useEffect(
     () => {
         const fetchMatches = async () => {
             const response = await fetch('http://localhost:8080/team/Chennai%20Super%20Kings');
             const data = await response.json();
-            console.log(data);
+            setTeam(data);
         }
 
         fetchMatches();
-    }
+    } , []  //Reason for empty array is first param initializes only first time and when the code changes not every ti,e
+    
   );
 
 
   return (
     <div className="TeamPage">
-      <h1>Team Name = Chennai Super Kings</h1>     
-      <MatchDetailCard/>
-      <MatchSmallCard/>
-      <MatchSmallCard/>
-      <MatchSmallCard/>
+      <h1>{team.teamName}</h1>     
+      <MatchDetailCard match = {team.matches[0]}/>
+      
+
+      {team.matches.slice(1).map(match => <MatchSmallCard match = {match} />)}   
+      {/* Slice remove 1st entry */}
 
     </div>
   );
