af54111641740d8b14834411ccbeb7ed67cf8e43
vishal <vishalambekar459@gmail.com>
2023-10-02 13:07:06 +0530

    match backedn api added

--- ipl-dashboard/src/main/java/com/ipl/controller/TeamController.java
+++ ipl-dashboard/src/main/java/com/ipl/controller/TeamController.java
@@ -1,11 +1,15 @@
 package com.ipl.controller;
 
-import org.springframework.data.domain.PageRequest;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ipl.model.Match;
 import com.ipl.model.Team;
 import com.ipl.repository.MatchRepository;
 import com.ipl.repository.TeamRepository;
@@ -22,7 +26,7 @@ public class TeamController {
 		this.teamRepository = teamRepository;
 		this.matchRepository = matchRepository;
 	}
-
+	
 	@GetMapping("/team/{teamName}")
 	public Team getTeam(@PathVariable String teamName) {
 		Team team =  this.teamRepository.findByTeamName(teamName);
@@ -30,5 +34,14 @@ public class TeamController {
 		//team.setMatches(this.matchRepository.findByTeamName(teamName, PageRequest.of(0, 3)));
 		
 		return team;
+	}
+	
+	
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMatchesForTeam(@PathVariable String teamName,@RequestParam int year){
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year+1, 1, 1);
+		
+		return this.matchRepository.findMatchesByTeamBetweenDates(teamName, startDate, endDate);
 	}
 }
--- ipl-dashboard/src/main/java/com/ipl/repository/MatchRepository.java
+++ ipl-dashboard/src/main/java/com/ipl/repository/MatchRepository.java
@@ -1,5 +1,6 @@
 package com.ipl.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
@@ -16,7 +17,9 @@ public interface MatchRepository extends CrudRepository<Match, Long>{
 	
 	public List<Match> findByTeam1OrTeam2OrderByDateDesc(String team1,String team2,Pageable pageable);  //same as above
 	
-	
+	@Query(nativeQuery = true, value = "Select * from Match where (team1 = ?1 or team2 = ?1) and date between ?2 AND ?3 order by date desc")
+	public List<Match> findMatchesByTeamBetweenDates(String teamName,LocalDate date1,LocalDate date2);
+	//public List<Match> findByTeam1OrTeam2AndDateBetweenOrderByDateDesc(String teamName1,String teamName2, LocalDate date1,LocalDate date2);
 	
 	default List<Match> findLatestMatchesByTeam(String teamName,int count){
 		return findByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
